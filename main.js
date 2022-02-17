81c046dbd13958bf226d1bc92af8bd0868c7f79c
RatushnyiArtem <92036528+RatushnyiArtem@users.noreply.github.com>
2022-02-17 17:48:40 +0100

    mathic

---
+++ main.js
@@ -0,0 +1,77 @@
+// Функция(function) ---> процедура
+// function name([params]) {
+//     инструкция
+// }
+// name([аргумент])
+
+// function sendMessage(){
+//     alert('Hello i learn Function')
+// }
+// sendMessage()
+// let name = prompt('Put your name') // глобальные переменные
+// let age = prompt('Put your age') // глобальные переменные
+// function message(name , age){
+//     // let name = 'Den' // локальные переменные
+//     // let age = '24' // локальные переменные
+//     let a = `Hello, your name ${name}, your age ${age}`
+//     alert(a)
+// }
+// let k = prompt('Put your name')
+// let t = +prompt('Put your age')
+// message(k , t)
+
+// Синтаксис функции(function) ----> функция
+// function name(params) {
+//     инструкция
+//     return результат
+// }
+// let a = name()
+// confirm(a)
+// function message(name , age){
+//     // let name = 'Den' // локальные переменные
+//     // let age = '24' // локальные переменные
+//     let a = `Hello, your name ${name}, your age ${age}`
+//     return a
+// }
+// let b = message('Den', 24)
+// alert(b)
+// function sum(a , b){
+//     c = a+b
+//     return c
+// }
+// let k = sum(2,2)
+// confirm(k )
+
+
+
+// MATH TASK 1
+// function display_message() {
+//     alert('I learn JavaScript theme function')
+// }
+// display_message()
+// MATH TASK 2
+// function favourite_book(title) {
+//     alert(`One of my favourite book is ${title}`)
+// }
+// let k = prompt('Put name of your favourite book')
+// favourite_book(k)
+// MATH TASK 3
+// let print = prompt('Введите текст который должен быть на футболке')
+// let size = prompt('Введите размер футболки')
+// function make_shirt() {
+//     alert(`Okey your size of t-shirt is ${size} , and you want to watch such print as ${print}`)
+// }
+// make_shirt()
+// MATH TASK 4
+// function make_shirt(size, text) {
+//     alert(`Okey your size of t-shirt is ${size} , and you want to watch such print as ${text}`)
+// }
+// let k = prompt('Введите размер своей футболки')
+// make_shirt(k, 'I love JavaScript')
+// MATH TASK 5
+// function describe_city(city , country){
+//     alert(`Last holiday I've been in ${city} city, which is located in ${country}`)
+// }
+// let k = prompt('Put in brakets city where are you in')
+// let c = prompt('Put in brakets country where this city is located')
+// describe_city(k,c)
